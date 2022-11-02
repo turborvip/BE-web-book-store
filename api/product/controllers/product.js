4479636fcd2d29972477d7c47af0049f01c8ed39
turborvip <turborvipjits>
2022-11-02 22:47:30 +0700

    update

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity, convertRestQueryParams } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -12,5 +12,26 @@ module.exports = {
 
     const entity = await strapi.services.product.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.product });
+  },
+  async findAllInHomePage(ctx) {
+    const page =  parseInt(ctx.query.page) || 1;
+    const pageSize =  parseInt(ctx.query.pageSize) || 20;
+
+    const productCount = await strapi.query("product").model.count();
+    let offset = (parseInt(page) - 1) * parseInt(pageSize);
+    let totalPage = Math.ceil(productCount / parseInt(pageSize));
+    const data = await strapi
+      .query("product")
+      .model.find({ status: 1 })
+      .select(["name", "description", "price", "quantity", "image"])
+      .limit(parseInt(pageSize))
+      .skip(offset);
+
+      const pagination = {
+        totalPage,
+        page,
+        pageSize,
+      }
+    return {data,pagination}
   },
 };
