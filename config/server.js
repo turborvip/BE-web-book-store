f4f366fdaa15191b5d2bb3b1526167e2916c6ec5
turborvip <turborvipjits>
2022-11-02 09:27:31 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c3bd80e29d5c7da149333aa58425357b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c1172a11-54c9-4566-af87-ab88d179136c'
+};
