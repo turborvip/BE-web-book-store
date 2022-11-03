b7dd0661c32f4fcb5c439762ce8235a1575c9f0c
turborvip <turborvipjits>
2022-11-03 11:07:11 +0700

    update

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity, convertRestQueryParams } = require("strapi-utils");
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -14,48 +14,54 @@ module.exports = {
     return sanitizeEntity(entity, { model: strapi.models.product });
   },
   async bestSeller(ctx) {
-    const page =  parseInt(ctx.query.page) || 1;
-    const pageSize =  parseInt(ctx.query.pageSize) || 20;
+    const page = parseInt(ctx.query.page) || 1;
+    const pageSize = parseInt(ctx.query.pageSize) || 20;
 
-    const productCount = await strapi.query("product").model.find({ status: 1 }).count();
-    let offset = await (parseInt(page) - 1) * parseInt(pageSize);
+    const productCount = await strapi
+      .query("product")
+      .model.find({ status: true })
+      .count();
+    let offset = (await (parseInt(page) - 1)) * parseInt(pageSize);
     let totalPage = await Math.ceil(productCount / parseInt(pageSize));
     const data = await strapi
       .query("product")
-      .model.find({ status: 1 })
+      .model.find({ status: true })
       .select(["name", "price", "quantity", "image"])
       .limit(parseInt(pageSize))
       .skip(offset)
-      .sort({'sold':'desc'})
-      const pagination = {
-        totalPage,
-        page,
-        pageSize,
-      }
-    return {data,pagination}
+      .sort({ sold: "desc" });
+    const pagination = {
+      totalPage,
+      page,
+      pageSize,
+    };
+    return { data, pagination };
   },
 
-  async productsByName(ctx) {
-    const page =  parseInt(ctx.query.page) || 1;
-    const pageSize =  parseInt(ctx.query.pageSize) || 20;
-
-    const productCount = await strapi.query("product").find({ status: 1, }).model.count();
-    let offset = await (parseInt(page) - 1) * parseInt(pageSize);
-    let totalPage = await Math.ceil(productCount / parseInt(pageSize));
-    const data = await strapi
-      .query("product")
-      .model.find({ status: 1 })
-      // .select(["name", "price", "quantity", "image"])
-      // .limit(parseInt(pageSize))
-      // .skip(offset)
-      .sort({'createdAt':'desc'})
-      const pagination = {
-        totalPage,
-        page,
-        pageSize,
-      }
-    return {data,pagination}
+  async productsByCategory(ctx) {
+    const category = ctx.query.category || null;
+    const page = parseInt(ctx.query.page) || 1;
+    const pageSize = parseInt(ctx.query.pageSize) || 20;
+    let offset = (await (parseInt(page) - 1)) * parseInt(pageSize);
+    const categoryId = await strapi
+      .query("category")
+      .model.find({ name: category, status: true })
+      .select(["id"]);
+    let data = [];
+    if (categoryId[0].id) {
+      data = await strapi
+        .query("product")
+        .model.find({ categories: { $elemMatch: { $eq: categoryId[0].id } } })
+        .limit(pageSize)
+        .skip(offset)
+        .select(["name", "price", "quantity", "image"])
+        .sort({ createdAt: "desc" });
+    }
+    const pagination = {
+      totalPage:data.length,
+      page,
+      pageSize,
+    };
+    return { data, pagination };
   },
-
-
 };
