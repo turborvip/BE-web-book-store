df80c98e9a19ccc5d36d397f73e47b4c9eaebd8c
turborvip <turborvipjits>
2022-11-03 08:50:17 +0700

    update

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -13,20 +13,20 @@ module.exports = {
     const entity = await strapi.services.product.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.product });
   },
-  async findAllInHomePage(ctx) {
+  async bestSeller(ctx) {
     const page =  parseInt(ctx.query.page) || 1;
     const pageSize =  parseInt(ctx.query.pageSize) || 20;
 
     const productCount = await strapi.query("product").model.count();
-    let offset = (parseInt(page) - 1) * parseInt(pageSize);
-    let totalPage = Math.ceil(productCount / parseInt(pageSize));
+    let offset = await (parseInt(page) - 1) * parseInt(pageSize);
+    let totalPage = await Math.ceil(productCount / parseInt(pageSize));
     const data = await strapi
       .query("product")
       .model.find({ status: 1 })
-      .select(["name", "description", "price", "quantity", "image"])
+      .select(["name", "price", "quantity", "image"])
       .limit(parseInt(pageSize))
-      .skip(offset);
-
+      .skip(offset)
+      .sort({'sold':'desc'})
       const pagination = {
         totalPage,
         page,
