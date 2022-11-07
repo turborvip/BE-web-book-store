7f4f146609921dcd26e5b8c49e2cabce84a037ef
turborvip <turborvipjits>
2022-11-07 11:26:15 +0700

    update

--- api/menu/controllers/menu.js
+++ api/menu/controllers/menu.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -6,5 +6,4 @@
  */
 
 module.exports = {
-
 };
